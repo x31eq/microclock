88bb9adf155722f6fb4fcdcd5edaaf449f5ce425
Graham Breed <gbreed@gmail.com>
2018-05-20 09:56:44 +0100

    Produce a byte array because it turns out the micro:bit can use that

--- microclock.py
+++ microclock.py
@@ -1,3 +1,5 @@
+IMAGE_STR_FORMATTER = ':'.join(['%s' * 5]*5)
+
 class Clock:
     skips = ((0xf, 0x1), (0x3c0, 0x40), (0xc000, -0xc000))
 
@@ -15,14 +17,17 @@ class Clock:
             if (self.stamp & mask) == mask:
                 self.stamp += fix
 
-    def image_str(self):
-        bright = {'0': '0', '1': str(self.brightness)}
-        helper = str(max(0, self.brightness - 4))
+    def image_bytes(self):
+        bright = {'0': b'\0', '1': bytes([self.brightness])}
+        helper = bytes([max(0, self.brightness - 4)])
         binary_str = f'{self.stamp:016b}'
         pix = [bright[pixel] for pixel in binary_str]
-        return '{}:{}:{}0{}0{}:0{}0:0{}0'.format(
+        return b'%s%s%s\0%s\0%s\0%s\0\0%s\0' % (
                 pix[0] + pix[2] + pix[4] + pix[6] + pix[8],
                 pix[1] + pix[3] + pix[5] + pix[7] + pix[9],
                 helper, helper, helper,
                 pix[10] + pix[12] + pix[14],
                 pix[11] + pix[13] + pix[15])
+
+    def image_str(self):
+        return IMAGE_STR_FORMATTER % tuple(self.image_bytes())
