3faed9671ce1661eed01c81dee780b106dc10313
Graham Breed <gbreed@gmail.com>
2018-05-20 20:34:07 +0100

    Saved some lines

--- microclock.py
+++ microclock.py
@@ -1,4 +1,3 @@
-
 class Clock:
     skips = ((0xf, 0x1), (0x3c0, 0x40), (0xc000, -0xc000))
 
@@ -17,5 +16,4 @@ class Clock:
         guide = bytes([max(0, self.bright - 4)])
         px = bytes(bright[pixel] for pixel in f'{self.stamp:016b}')
         return px[:9:2] + px[1:10:2] + b'%s\0%s\0%s\0%s\0\0%s\0' % (
-                guide, guide, guide,
-                px[10::2], px[11::2])
+                guide, guide, guide, px[10::2], px[11::2])
