8f43325553892ecad1f14cde2c11f1ee0a6fd1f5
Graham Breed <gbreed@gmail.com>
2018-05-20 07:20:33 +0100

    Ticking clock for testing (keeps bad time)

--- test.py
+++ test.py
@@ -5,6 +5,35 @@ Desktop tests for MicroPython code
 """
 
 import microclock
+import time
+
+
+def tick_hex(start=None, wait=1000):
+    if start is None:
+        start = unix_to_micro()
+    clock = microclock.Clock(start)
+    while True:
+        clock.tick()
+        print('%04x' % clock.stamp)
+        time.sleep(wait / 1000)
+
+
+def unix_to_micro(unix_stamp=None):
+    if unix_stamp is None:
+        unix_stamp = time.time()
+    time_struct = time.localtime(unix_stamp)
+    return hms_to_micro(
+            time_struct.tm_hour,
+            time_struct.tm_min,
+            time_struct.tm_sec)
+
+
+def hms_to_micro(hour, minute, sec):
+    result = hour << 12
+    result += (minute + minute // 15) << 6
+    result += sec + sec // 15
+    return result
+
 
 if __name__ == '__main__':
     clock = microclock.Clock()
@@ -26,7 +55,7 @@ if __name__ == '__main__':
         assert clock.stamp & 0x3f == 0, clock.stamp
     assert clock.stamp == 0x1000, clock.stamp
 
-    clock = microclock.Clock(0xb000)
+    clock = microclock.Clock(hms_to_micro(11, 0, 0))
     for minute in range(60):
         for sec in range(60):
             clock.tick()
