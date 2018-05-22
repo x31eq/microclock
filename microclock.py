2c3f00ff98ecfa932b22cca8d8f5621e65fe247c
Graham Breed <gbreed@gmail.com>
2018-05-22 21:00:44 +0100

    microclock that works with the microbit

--- microclock.py
+++ microclock.py
@@ -16,10 +16,10 @@ class Clock:
         guide = bytes([max(0, self.bright - 4)])
         binary = '{:016b}'.format(self.stamp)
         px = bytes(bright[pixel] for pixel in binary)
-        return b'%s%s%s\0%s\0%s\0%s\0\0%s\0' % (
-                px[0] + px[2] + px[4] + px[6] + px[8],
-                px[1] + px[3] + px[5] + px[7] + px[9],
+        return b'%s%s%s%s%s%s%s%s%s%s%s\0%s\0%s\0%s%s%s\0\0%s%s%s\0' % (
+                px[0:1], px[2:3], px[4:5], px[6:7], px[8:9],
+                px[1:2], px[3:4], px[5:6], px[7:8], px[9:10],
                 guide, guide, guide,
-                px[10] + px[12] + px[14],
-                px[11] + px[13] + px[15])
+                px[10:11], px[12:13], px[14:15],
+                px[11:12], px[13:14], px[15:16])
 
