da54f3af2a1994defc65a8c642d7217ab72f7c4e
Graham Breed <gbreed@gmail.com>
2018-05-22 23:44:29 +0100

    Build bytes from an integer array

--- microclock.py
+++ microclock.py
@@ -12,22 +12,22 @@ class Clock:
                 self.stamp += fix
 
     def image_b(self):
-        bright = {'0': 0, '1': self.bright}
-        guide = bytes([max(0, self.bright - 4)])
+        bright = bytes((0, self.bright))
+        guide = max(1, self.bright - 3)
         binary = '{:016b}'.format(self.stamp)
-        px = bytes(bright[pixel] for pixel in binary)
-        return b'%s%s%s%s%s%s%s%s%s%s%s\0%s\0%s\0%s%s%s\0\0%s%s%s\0' % (
-                px[0:1], px[2:3], px[4:5], px[6:7], px[8:9],
-                px[1:2], px[3:4], px[5:6], px[7:8], px[9:10],
-                guide, guide, guide,
-                px[10:11], px[12:13], px[14:15],
-                px[11:12], px[13:14], px[15:16])
+        px = bytes(bright[pixel == '1'] for pixel in binary)
+        return bytes((
+                px[0], px[2], px[4], px[6], px[8],
+                px[1], px[3], px[5], px[7], px[9],
+                guide, 0, guide, 0, guide,
+                0, px[10], px[12], px[14], 0,
+                0, px[11], px[13], px[15], 0))
 
 
 def run(start=0):
     from microbit import display, Image, running_time, sleep
     now = running_time()
-    clock = Clock(start, 6)
+    clock = Clock(start, 5)
     while True:
         display.show(Image(5, 5, clock.image_b()))
         now += 1000
